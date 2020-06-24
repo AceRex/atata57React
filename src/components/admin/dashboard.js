3007f8df064606a9b34022a1706ee34eda70d37b
Are Oluwasegun <37227404+AceRex@users.noreply.github.com>
2020-06-24 07:27:28 +0100

    enter

--- src/components/admin/dashboard.js
+++ src/components/admin/dashboard.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import { Tab } from 'semantic-ui-react'
 import "./dashboard.css"
-import { Loader, Paragraph } from 'rsuite';
+import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react'
+
 
 const panes = [
   {
     menuItem: 'Main', render: () => <Tab.Pane>
-      <Paragraph rows={8}>
-        <Loader size="md" center content="loading" />
-      </Paragraph>
+      <Segment>
+        <Dimmer active inverted>
+          <Loader size='large'>Loading</Loader>
+        </Dimmer>
+
+        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+      </Segment>
   </Tab.Pane> },
   { menuItem: 'Blog', render: () => <Tab.Pane></Tab.Pane> },
-  { menuItem: 'Placeholder', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+  {
+    menuItem: 'Placeholder', render: () => <Tab.Pane><Segment>
+      <Dimmer active inverted>
+        <Loader size='large'>Preparing Files</Loader>
+      </Dimmer>
+
+      <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+    </Segment></Tab.Pane> },
 ]
 
 function Dashboard() {
